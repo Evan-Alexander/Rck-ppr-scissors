119d03094f50423fcea0209c02d63d606a1ca989
Jason Brown and Koji Nakagawa <jason+koji@>
2017-02-15 09:22:31 -0800

    add rock and scissors case: spec1 pass successfully

---
+++ src/RockPaperScissors.php
@@ -0,0 +1,53 @@
+<?php
+
+    class RockPaperScissors
+    {
+        // function playGame($input1, $input2) //$input1: rock, $input2:scissors
+        // {
+        //
+        //         $getShowHands = showHands($input1, $input2); //"rock"
+        //
+        //         winning check function
+        //             if "draw" -> draw
+        //
+        //             elseif $input1 == rock -> player1 win
+        //             elseif $input2 == rock -> player2 win
+        //
+        //         // returning result function
+        //         // if $input1 and $input2 include rock and scissors
+        //         //
+        //         //     case; ($input1==rock and $input2==scissors  ||
+        //         //           $input1==scissors and $input2 == rock);
+        //         //           ->return rock
+        //         //
+        //         // if $input1 and $input2 include scissors and papers
+        //         //     ->return scissors
+        //         // if $input1 and $input2 include rock and papers
+        //         //     ->return paper
+        //         // if $input1 == $input2
+        //         //     ->return draw
+        //         //
+        //         //
+        //
+        //         //
+        //         //     elseif $input1 == scissors -> player1 win
+        //         //     elseif $input2 == scissors -> player2 win
+        //         //
+        //         //     elseif $input1 == scissors -> player1 win
+        //         //     elseif $input2 == scissors -> player2 win
+        //         //
+        // }
+
+
+        function playGame($input1, $input2)
+        {
+            if (($input1 == "rock" && $input2 == "scissors") || ($input1 == "scissors" && $input2 == "rock")) {
+                if ($input1 == "rock") {
+                    return "Player 1";
+                } else{
+                    return "Player 2";
+                }
+            }
+        }
+
+    }
